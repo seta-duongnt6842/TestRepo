6928352516b1025c41bc0a4b4bd8b7faed404410
NotAsea <luonghaic1166@gmail.com>
2023-12-25 10:18:13 +0700

    dfsdfdsfsd

--- docker-push.sh
+++ docker-push.sh
@@ -1,3 +1,3 @@
 docker build -t haidoc78/test:latest -f TestRepo/Dockerfile .
-docker login -u HAIdoc78 -p huuhai@#$%2207
+docker login -u haidoc78 -p huuhai@#$%2207
 docker push haidoc78/test:latest
